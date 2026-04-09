9b1b94ebee65ce4856c91d337fdf2a5251b9600f
文强哥 <50829543+Johnny520@users.noreply.github.com>
2026-04-09 22:55:00 +0800

    更新 build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,16 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    // 这里保留你原本有的插件，通常是下面这两个
+    id("com.android.application") version "8.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
 
-// 这个文件只负责管理插件版本，不需要其他内容
+// --- 修复开始：替换掉报错的清理和配置代码 ---
+tasks.register("clean", Delete::class) {
+    delete(rootProject.layout.buildDirectory)
+}
+// --- 修复结束 ---
+
+// 如果下面还有 allprojects 或 subprojects 块，请保持原样不动
+// 如果没有，保持上面的代码即可
