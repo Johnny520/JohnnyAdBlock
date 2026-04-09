f471012398649fcfe0715330411b5b4469e66007
文强哥 <50829543+Johnny520@users.noreply.github.com>
2026-04-09 22:41:23 +0800

    更新 build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,11 @@
 import org.gradle.api.tasks.Delete
 
 plugins {
-    // 保持你原有的插件配置，这里根据你的项目推测
     id("com.android.application") version "8.2.0" apply false
     id("com.android.library") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
 
-// 修复报错：Unresolved reference 和 Deprecated buildDir
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
