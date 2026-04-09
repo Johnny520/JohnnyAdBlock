5401aeef18979503e0364e5d7f552f49faf9ef38
文强哥 <50829543+Johnny520@users.noreply.github.com>
2026-04-09 22:13:01 +0800

    更新 build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,24 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
 }
 
+// 全局禁用 Kotlin 构建统计服务，彻底解决 buildFlowServiceProperty 报错
+allprojects {
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        kotlinOptions {
+            freeCompilerArgs += "-Xno-param-assertions"
+        }
+    }
+    plugins.withId("org.jetbrains.kotlin.android") {
+        (this as org.jetbrains.kotlin.gradle.plugin.KotlinAndroidPluginWrapper)
+            .extension
+            .let { ext ->
+                ext.explicitApiMode = org.jetbrains.kotlin.gradle.dsl.ExplicitApiMode.Disabled
+                // 核心：禁用构建统计服务
+                ext.buildFlowServiceEnabled.set(false)
+            }
+    }
+}
+
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
